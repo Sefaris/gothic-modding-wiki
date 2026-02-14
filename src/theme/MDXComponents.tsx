8b46deed5cd7a58bad4e4efa11c2dd566c4aa692
Franisz <joannit7@gmail.com>
2026-02-14 01:20:18 +0100

    feat: add @iconify/react for icon support

---
+++ src/theme/MDXComponents.tsx
@@ -0,0 +1,9 @@
+import MDXComponents from "@theme-original/MDXComponents";
+import { Icon } from "@iconify/react";
+
+const components = {
+  ...MDXComponents,
+  IIcon: Icon,
+};
+
+export default components;
